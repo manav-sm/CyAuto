77a689e47d22de3bd7e28ae01dba994bd6104dcc
Manav Chokshi <manav@simformsolutions.com>
2023-04-28 18:35:46 +0530

    BDDWithHTMLReport

---
+++ cypress/Integration/Example/BDD/eComm.feature
@@ -0,0 +1,12 @@
+Feature: EComm E2E Testign
+
+    E2E Testign
+
+        Scenario: Ecommerce Delivery
+        Given the User navigate to Website
+        When the User adds items in the cart
+        And the use validate the total price
+        Then the user enters coutry name and verify the message
+
+
+
---
+++ cypress/Integration/Example/BDD/eComm/eCommStepDef.js
@@ -0,0 +1,50 @@
+import { Given,When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import homePage from "../../../../pageObject/homePage"
+
+const hm = new homePage()
+Given('the User navigate to Website',() => {
+    cy.visit(Cypress.env('url')+"/angularpractice/")
+});
+
+When('the User adds items in the cart',function(){
+    
+    
+    hm.userName().type(this.data.name)
+    hm.userEmail().type(this.data.email)
+    hm.productSelection().select(this.data.gender)
+    hm.userName().should('have.attr','minlength','2')
+    hm.enterprenuerRedioBtnLoc().should('be.disabled')
+    hm.shopBtnLoc().click()
+    this.data.productSelection.forEach(function(element) {
+        cy.productSelection(element)
+    });
+    cy.get('.nav-link.btn.btn-primary').click()
+})
+
+When('the use validate the total price',function(){
+    var sum = 0
+    cy.get('tr td:nth-child(4) strong').each(($el,index,$list) => {
+        var productPrice = $el.text().split(" ")[1].trim()
+        cy.log(Number(productPrice))
+        sum = sum + Number(productPrice)
+    }).then(function(){
+        cy.log(sum)
+    })
+    cy.get('h3 > strong').then(function(element){
+        const total = element.text().split(" ")[1].trim()
+        expect(sum).to.equal(Number(total))
+    })
+})
+
+Then('the user enters coutry name and verify the message',function(){
+    cy.get('button[class="btn btn-success"]').click()
+    cy.get('#country').type("india")
+    cy.get('.suggestions > ul > li > a').click()
+    cy.get('#checkbox2').click({force: true})
+    cy.get('input[value="Purchase"]').click()
+    cy.get('.alert').then(function(element){
+        expect(element.text().includes("Success")).to.be.true
+    })
+})
+
+
